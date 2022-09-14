05d979a53efb8f98c88dbc1b65aa2e3dd5017a30
MadScientist586 <mr.scientist.knight@gmail.com>
2022-09-14 15:03:04 -0500

    made number guessing game

---
+++ If Else/Program.cs
@@ -0,0 +1,21 @@
+﻿var r = new Random();
+var favNumber = r.Next(1, 1000);
+while (true)
+{
+    Console.WriteLine("Guess the number:");
+    var userInput = int.Parse(Console.ReadLine());
+
+
+    if (userInput < favNumber)
+    {
+        Console.WriteLine("too low");
+    }
+    else if (userInput > favNumber)
+    {
+        Console.WriteLine("too high");
+    }
+    else if (userInput == favNumber)
+    {
+        Console.WriteLine("You guessed it!!!!");
+    }
+}
